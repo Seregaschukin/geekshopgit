3c78edb0481ab547e9ac4e5d73d14b858d55c91b
ser <seregaschukin96@mail.ru>
2021-09-21 22:53:52 +0300

    added test.html

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from datetime import datetime
 
 # Create your views here.
 
@@ -7,3 +8,20 @@ def index(request):
 
 def products(request):
     return render(request, 'mainapp/products.html')
+
+def test(request):
+    context = {
+        'title': 'geekshop',
+        'header': 'Добро пожаловать',
+        'user': 'Сергей',
+        'date': datetime.today(),
+        'products': [{'name': 'Худи черного цвета с монограммами adidas Originals', 'price': '6 090,00'},
+                    {'name': 'Синяя куртка The North Face', 'price': '23 725,00'},
+                    {'name': 'Коричневый спортивный oversized-топ ASOS DESIGN', 'price': '3 390,00'},
+
+    ],
+        'products_promo': [{'name': 'Черный рюкзак Nike Heritage', 'price': '5864'},
+                     {'name': 'Черные туфли на платформе с 3 парами люверсов Dr Martens 1461 Bex', 'price': '21111'}
+    ]
+    }
+    return render(request, 'mainapp/test.html', context)
